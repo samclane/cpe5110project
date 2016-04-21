9420c4ed243f89a7e1d56f2d05711cd06b1c1659
samclane <samclane@gmail.com>
2016-04-21 13:29:57 -0500

    can now extract full integers from the code. Using '+=' in core loop gives no errors

---
+++ History.md
@@ -0,0 +1,7 @@
+
+n.n.n / 2016-04-21
+==================
+
+ * Can read in file, and begin to find ints within file
+ * First commit
+ * Initial commit
--- main.cpp
+++ main.cpp
@@ -6,37 +6,45 @@
 #include <fstream>
 #include <vector>
 using namespace std;
+const int INPUT_BUFFER_SIZE = 10000;
 
 
 int main (int argc, char** argv)
 {
-	string inputBuffer[10000];
-	string temp;
+	string inputBuffer[INPUT_BUFFER_SIZE];
+	string temp_line;
 	int instrCount, memoryCount;
 	int index;
 	ifstream in_stream;
+	if (argc != 2)
+	{
+		cerr << "Please input a file name" << endl;
+		return 0;
+	}
 	try 
 	{
 		in_stream.open(argv[1]);
 	}
 	catch (ifstream::failure e) 
 	{
+		//never executes
 		cerr << "Exception reading/opening file\n";
 	}
 	while(!in_stream.eof())
 	{
-		getline(in_stream,temp);
-		if (48 <= ((temp[0]-'0') + 48) && ((temp[0]-'0') + 48) <= 57)
+		getline(in_stream,temp_line); //gets next line from file
+		if (48 <= ((temp_line[0]-'0') + 48) && ((temp_line[0]-'0') + 48) <= 57)
 		{
 			//first value is a number
 			int index2 = 0;
 			string tempNum = "";
-			while(temp[index2] != ' ')
+			while(temp_line[index2] != ' ')
 			{
-				strcat(tempNum, temp[index2]);
+				tempNum += temp_line[index2];
+				index2++;
 			}
-			instrCount = tempNum;
-			cout << "myval " << ((temp[0]-'0') + 48) << endl;
+			instrCount = atoi(tempNum.c_str());
+			cout << "found int: " << ((temp_line[0]-'0') + 48) << endl;
 		} 
 		index++;
 	}
