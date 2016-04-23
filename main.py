a6991e1962f92a698406fe50e008824491dbf370
samclane <samclane@gmail.com>
2016-04-23 16:29:33 -0500

    Now sorts instructions by type. Access by instruction.get_type().'

--- main.py
+++ main.py
@@ -12,13 +12,22 @@ class Instruction():
 		self.operands = []
 		self.opcode = opcode
 		for operand in argv:
-			self.operands.append(operand.strip(','))
+			self.operands.append(operand.strip(',#'))
+		if self.opcode[0] == 'B':
+			self.type = "BRANCH"
+		elif self.opcode[:2] == "FP":
+			self.type = "FLOATING_POINT"
+		else:
+			self.type = "INTEGER"
 
 	def get_opcode(self):
 		return self.opcode
 
 	def get_operands(self):
 		return self.operands
+
+	def get_type(self):
+		return self.type
 
 
 def main(argv):
@@ -54,6 +63,7 @@ def main(argv):
 	print mem_list
 	for instruction in instr_list:
 		print "Opcode: " + instruction.get_opcode()
+		print "Type: " + instruction.get_type()
 		for idx, operand in enumerate(instruction.get_operands()):
 			print "Operand " + str(idx) + ": " + operand
 
